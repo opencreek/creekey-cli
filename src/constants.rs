3e195c31a8b8c430d125cb074488f6bb9be40458
Mischa Holz <me@mhlz.de>
2026-05-13 22:13:30 +0200

    Use better paths for sockets
    Invoking creekey as sudo can leave the agent in a stuck state where it
    tries to read from a socket owned by root. This causes creekey to hang indefinitely.
    
    This is fixed by using a different socket path per user and also
    introducing a timeout for the agent to connect.

--- src/constants.rs
+++ src/constants.rs
@@ -15,3 +15,41 @@ pub fn get_ssh_key_path() -> Result<PathBuf> {
     path.push(SSH_KEY_PATH);
     Ok(path)
 }
+
+pub fn current_uid() -> u32 {
+    unsafe { libc::getuid() }
+}
+
+fn per_user_runtime_dir() -> Option<PathBuf> {
+    if let Some(dir) = dirs::runtime_dir() {
+        return Some(dir);
+    }
+    if cfg!(target_os = "macos") {
+        if let Some(tmpdir) = std::env::var_os("TMPDIR") {
+            return Some(PathBuf::from(tmpdir));
+        }
+    }
+    None
+}
+
+fn runtime_path(base: &str, ext: &str) -> String {
+    match per_user_runtime_dir() {
+        Some(mut path) => {
+            path.push(format!("{}.{}", base, ext));
+            path.to_string_lossy().into_owned()
+        }
+        None => format!("/tmp/{}-{}.{}", base, current_uid(), ext),
+    }
+}
+
+pub fn agent_socket_path() -> String {
+    runtime_path("ck-ssh-agent", "sock")
+}
+
+pub fn agent_pid_path() -> String {
+    runtime_path("ck-agent", "pid")
+}
+
+pub fn logger_socket_path(random: &str) -> String {
+    runtime_path(&format!("ck-logger-{}", random), "sock")
+}
